1c7dc55fe7afeccb656ff29f9819966081233d6d
ryanbae94 <ryanbae94@gmail.com>
2024-09-17 16:49:16 +0900

    chore: manifest 설정(#2)

---
+++ src/app/manifest.ts
@@ -0,0 +1,27 @@
+import { MetadataRoute } from 'next'
+
+const manifest = (): MetadataRoute.Manifest => {
+  return {
+    name: '톡픽(TokPik)',
+    short_name: 'TokPik',
+    description: '톡픽 테스트',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#000',
+    theme_color: '#000',
+    icons: [
+      {
+        src: '/favicons/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/favicons/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
+
+export default manifest
