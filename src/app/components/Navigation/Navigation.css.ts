4d197f51e3a967078d4944906a35128f2ddc28f6
ryanbae94 <ryanbae94@gmail.com>
2024-09-19 21:13:06 +0900

    feat: navigation 구현

---
+++ src/app/components/Navigation/Navigation.css.ts
@@ -0,0 +1,24 @@
+import { style } from '@vanilla-extract/css'
+
+export const Container = style({
+  position: 'absolute',
+  top: 'calc(100% - 60px)',
+  width: '100vw',
+  display: 'flex',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+  backgroundColor: '#333333',
+  height: '60px',
+  borderRadius: '15px 15px 0 0',
+})
+
+export const ButtonWrapper = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '4px',
+})
+
+export const ButtonText = style({
+  fontSize: '12px',
+})
