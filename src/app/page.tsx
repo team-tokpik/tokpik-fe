f3bc8c0ff09f8a44708cd549ec58e7afc1178b10
ryanbae94 <ryanbae94@gmail.com>
2024-09-22 15:05:47 +0900

    chore: 테스트 링크 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 'use client'
 import Navigation from '@/components/Navigation/Navigation'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <>
+      <Link href="/survey/gender-birth">생년월일 성별 페이지</Link>
       <Navigation />
     </>
   )
