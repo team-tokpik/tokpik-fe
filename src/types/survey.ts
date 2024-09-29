1566ecb38b1c3e988ffc1602164187ed0a4bf4d5
ryanbae94 <ryanbae94@gmail.com>
2024-09-30 00:31:44 +0900

    feat: post profile을 위한 type 설정

--- src/types/survey.ts
+++ src/types/survey.ts
@@ -15,3 +15,16 @@ export type topicTags = {
   topicTagId: number
   content: string
 }
+
+/*
+  birth: "YYYY-MM-DD" 형식
+  gender: true: 남자, false: 여자
+  placeTags: placeTagId 배열
+  topicTags: topicTagId 배열
+*/
+export type ProfileRequest = {
+  birth: string
+  gender: boolean
+  placeTags: number[]
+  topicTags: number[]
+}
