5a11d68651c278c15b75eef22943c26d3abd7508
JeongBin Park <obb8923@naver.com>
2024-09-30 22:13:25 +0900

    feat: 사용자 스크랩수 api (#13)

---
+++ src/api/my/getUserTalksCount.ts
@@ -0,0 +1,15 @@
+import client from '../client';
+
+interface TalksCountResponse {
+  count: number;
+}
+
+export const getUserTalksCount = async (): Promise<number> => {
+  try {
+    const response = await client.get<TalksCountResponse>('/users/scraps/tokpik/count');
+    return response.data.count;
+  } catch (error) {
+    console.error('스크랩 수 조회 중 오류 발생:', error);
+    throw error;
+  }
+};
