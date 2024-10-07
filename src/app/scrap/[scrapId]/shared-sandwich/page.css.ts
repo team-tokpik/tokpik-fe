c4f2fb6b5a48e4fbaea0aa92721121f313b8e027
ryanbae94 <ryanbae94@gmail.com>
2024-10-07 16:10:07 +0900

    feat: 공유하기 눌렀을 때 리다이렉트 되는 페이지

---
+++ src/app/scrap/[scrapId]/shared-sandwich/page.css.ts
@@ -0,0 +1,58 @@
+import { vars } from '@/app/globals.css'
+import { style } from '@vanilla-extract/css'
+
+export const container = style({
+  width: '100%',
+  minHeight: '100vh',
+})
+
+export const wrapper = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '2rem',
+})
+
+export const captureArea = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '2rem',
+  width: '100%',
+  backgroundColor: vars.color.black,
+})
+
+export const imageContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '0.63rem',
+})
+
+export const title = style({
+  marginTop: '1.88rem',
+  color: vars.color.white,
+  fontSize: '1.75rem',
+  fontWeight: 600,
+  textAlign: 'center',
+})
+
+export const fixedContainer = style({
+  height: '5.5rem',
+})
+
+export const buttonContainer = style({
+  position: 'fixed',
+  bottom: '0',
+  display: 'flex',
+  color: vars.color.white,
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  paddingBottom: '40px',
+  gap: '0.94rem',
+  width: '80%',
+})
