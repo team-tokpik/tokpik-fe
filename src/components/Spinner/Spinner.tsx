7fd49db694b52526893a72a6a7ac69e9aafb937e
ryanbae94 <ryanbae94@gmail.com>
2024-10-07 16:11:41 +0900

    fixed: 빌드 에러 수정

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,15 +1,14 @@
 import * as styles from './Spinner.css'
 import Square from '../Square/Square'
 import { SpinnerProps } from '@/types/spinnerProps'
-import { vars } from '@/app/globals.css'
 const Spinner = ({ text }: SpinnerProps) => {
   return (
     <div className={styles.OuterContainer}>
       <div className={styles.SpinnerContainer}>
-        <Square className={styles.Square1} color="primary"/>
-        <Square className={styles.Square2} color="primary"/>
-        <Square className={styles.Square3} color="primary"/>
-        <Square className={styles.Square4} color="primary"/>
+        <Square className={styles.Square1} color="primary" />
+        <Square className={styles.Square2} color="primary" />
+        <Square className={styles.Square3} color="primary" />
+        <Square className={styles.Square4} color="primary" />
       </div>
       <p className={styles.LoadingText}>조금만 기다려주세요</p>
       {text && <div className={styles.Text}>{text}</div>}
