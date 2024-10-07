7e9517abfca5d0f718ab8fe9df3844069669571c
ryanbae94 <ryanbae94@gmail.com>
2024-10-07 16:11:03 +0900

    refactor: 백바 뒤로가기 기능 추가

--- src/components/BackBar/BackBar.tsx
+++ src/components/BackBar/BackBar.tsx
@@ -2,15 +2,20 @@ import React from 'react'
 import * as styles from './BackBar.css'
 import Back from '/public/images/Back.svg'
 import Close from '/public/images/Close.svg'
+import { useRouter } from 'next/navigation'
 type BackBarProps = {
   isClose?: boolean
   label?: string
 }
 
 export default function BackBar({ isClose = false, label }: BackBarProps) {
+  const router = useRouter()
+  const handleBack = () => {
+    router.back()
+  }
   return (
     <div className={styles.wrapper}>
-      <Back />
+      <Back onClick={handleBack} />
       {label && <p>{label}</p>}
       {isClose && <Close />}
     </div>
