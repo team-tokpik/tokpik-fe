a9ae8f7460740fbcde209b0aa8cb23944ebd8a53
ryanbae94 <ryanbae94@gmail.com>
2024-10-07 16:11:24 +0900

    feat: 버튼에 color props 추가

--- src/types/button.ts
+++ src/types/button.ts
@@ -1,7 +1,8 @@
 export type ButtonType = {
   size: 'small' | 'medium' | 'large'
   label: string
-  active: boolean
+  active?: boolean
   onClick: () => void
   disabled?: boolean
+  color?: 'primary' | 'secondary'
 }
