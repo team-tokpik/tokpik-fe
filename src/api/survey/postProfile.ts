53147716d7357ef8a602032ca2075d64c8a9da14
ryanbae94 <ryanbae94@gmail.com>
2024-10-10 01:21:25 +0900

    fix: profile api 경로 수정

--- src/api/survey/postProfile.ts
+++ src/api/survey/postProfile.ts
@@ -2,7 +2,7 @@ import { ProfileRequest } from '@/types/survey'
 import client from '../client'
 
 export const postProfile = async (data: ProfileRequest) => {
-  const response = await client.post('/users/profile', {
+  const response = await client.post('/users/profiles', {
     gender: data.gender,
     birth: data.birth,
     placeTagIds: data.placeTags,
